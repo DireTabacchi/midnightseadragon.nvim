caad7c63bfaa894f1158daf8858c9f9ea4533b2e
Nathaniel Tabacchi <ntabacchi@outlook.com>
2024-12-16 17:36:06 -0500

    Improved lualine support for git symbols. Added gitsigns support.

--- lua/lualine/themes/midnightseadragon.lua
+++ lua/lualine/themes/midnightseadragon.lua
@@ -1,5 +1,5 @@
+local c_util = require("midnightseadragon.color_util")
 local colors = require("midnightseadragon.colors").default
-local util = require("midnightseadragon.util")
 
 return {
   normal = {
@@ -9,28 +9,28 @@ return {
     z = { bg = colors.bg4, fg = colors.fg0 }
   },
   insert = {
-    a = { bg = colors.green_l, fg = util.darken(colors.green_d, 0.30), gui = 'bold' },
-    b = { bg = util.darken(colors.green_d, 0.30), fg = colors.green_l },
+    a = { bg = colors.green_l, fg = c_util.darken(colors.green_d, 0.30), gui = 'bold' },
+    b = { bg = c_util.darken(colors.green_d, 0.30), fg = colors.green_l },
     c = { bg = colors.bg1, fg = colors.green_d },
-    z = { bg = colors.green_l, fg = util.darken(colors.green_d, 0.30) }
+    z = { bg = colors.green_l, fg = c_util.darken(colors.green_d, 0.30) }
   },
   visual = {
-    a = { bg = colors.orange_l, fg = util.darken(colors.orange_d, 0.30), gui = 'bold' },
-    b = { bg = util.darken(colors.orange_d, 0.30), fg = colors.orange_l },
+    a = { bg = colors.orange_l, fg = c_util.darken(colors.orange_d, 0.30), gui = 'bold' },
+    b = { bg = c_util.darken(colors.orange_d, 0.30), fg = colors.orange_l },
     c = { bg = colors.bg1, fg = colors.orange_d },
-    z = { bg = colors.orange_l, fg = util.darken(colors.orange_d, 0.30) }
+    z = { bg = colors.orange_l, fg = c_util.darken(colors.orange_d, 0.30) }
   },
   replace = {
-    a = { bg = colors.red_l, fg = util.darken(colors.red_d, 0.30), gui = 'bold' },
-    b = { bg = util.darken(colors.red_d, 0.30), fg = colors.red_l },
+    a = { bg = colors.red_l, fg = c_util.darken(colors.red_d, 0.30), gui = 'bold' },
+    b = { bg = c_util.darken(colors.red_d, 0.30), fg = colors.red_l },
     c = { bg = colors.bg1, fg = colors.red_d },
-    z = { bg = colors.red_l, fg = util.darken(colors.red_d, 0.30) },
+    z = { bg = colors.red_l, fg = c_util.darken(colors.red_d, 0.30) },
   },
   command = {
-    a = { bg = colors.seafoam_l, fg = util.darken(colors.seafoam_d, 0.30), gui = 'bold' },
-    b = { bg = util.darken(colors.seafoam_d, 0.30), fg = colors.seafoam_l },
+    a = { bg = colors.seafoam_l, fg = c_util.darken(colors.seafoam_d, 0.30), gui = 'bold' },
+    b = { bg = c_util.darken(colors.seafoam_d, 0.30), fg = colors.seafoam_l },
     c = { bg = colors.bg1, fg = colors.seafoam_d },
-    z = { bg = colors.seafoam_l, fg = util.darken(colors.seafoam_d, 0.30) },
+    z = { bg = colors.seafoam_l, fg = c_util.darken(colors.seafoam_d, 0.30) },
   },
   inactive = {
     a = { bg = colors.bg2, fg = colors.fg2, gui = 'bold' },
---
+++ lua/midnightseadragon/color_util.lua
@@ -0,0 +1,94 @@
+local M = {}
+
+--- HEXtoHSB converts a hex colorcode to its HSB equivalent.
+--- color is a string with a # followed by six hex digits (as in '#RRGGBB', where R, G, B are
+--- hex digits). The returned value is a table with the keys H, S, B, which correspond to the Hue,
+--- Saturation, and Brightness, respectively.
+--@return {H number, S number, B number}
+local function HEXtoHSB(color)
+  local red = tonumber(string.sub(color, 2, 3), 16) / 255
+  local green = tonumber(string.sub(color, 4, 5), 16) / 255
+  local blue = tonumber(string.sub(color, 6, 7), 16) / 255
+
+  local brightness = math.max(red, green, blue)
+
+  local chroma = brightness - math.min(red, green, blue)
+  local hue
+
+  if chroma == 0 then
+    hue = 0
+  elseif brightness == red then
+    hue = 60 * (((green - blue) / chroma) % 6)
+  elseif brightness == green then
+    hue = 60 * (((blue - red) / chroma) + 2)
+  elseif brightness == blue then
+    hue = 60 * (((red - green) / chroma) + 4)
+  end
+
+  local saturation
+  if brightness == 0 then
+    saturation = 0
+  else
+    saturation = chroma / brightness
+  end
+
+  return { h = hue, s = saturation, b = brightness }
+end
+
+local function HSBtoHEX(hsb)
+  local chroma = hsb.b * hsb.s
+  local hue_p = hsb.h / 60
+  local x = chroma * (1 - math.abs(hue_p % 2 - 1))
+  local i_rgb = {}
+  if hue_p < 1 and hue_p >= 0 then
+    i_rgb = { r = chroma, g = x, b = 0 }
+  elseif hue_p < 2 and hue_p >= 1 then
+    i_rgb = { r = x, g = chroma, b = 0 }
+  elseif hue_p < 3 and hue_p >= 2 then
+    i_rgb = { r = 0, g = chroma, b = x }
+  elseif hue_p < 4 and hue_p >= 3 then
+    i_rgb = { r = 0, g = x, b = chroma }
+  elseif hue_p < 5 and hue_p >= 4 then
+    i_rgb = { r = x, g = 0, b = chroma }
+  elseif hue_p < 6 and hue_p >= 5 then
+    i_rgb = { r = chroma, g = 0, b = chroma }
+  end
+
+  local m = hsb.b - chroma
+  i_rgb.r = i_rgb.r + m
+  i_rgb.g = i_rgb.g + m
+  i_rgb.b = i_rgb.b + m
+
+  local hex = string.format("#%02X%02X%02X",
+    math.floor(i_rgb.r * 255),
+    math.floor(i_rgb.g * 255),
+    math.floor(i_rgb.b * 255))
+
+  return hex
+end
+
+-- darken subtracts percent from an HSB Brightness value of color and returns the hex color code
+-- of that darkened color.
+function M.darken(color, percent)
+  local color_hsb = HEXtoHSB(color)
+  local diff = color_hsb.b - percent
+  if diff <= 0.0 then
+    color_hsb.b = 0.0
+  else
+    color_hsb.b = diff
+  end
+  return HSBtoHEX(color_hsb)
+end
+
+function M.brighten(color, percent)
+  local color_hsb = HEXtoHSB(color)
+  local diff = color_hsb.b + percent
+  if diff >= 1.0 then
+    color_hsb.b = 1.0
+  else
+    color_hsb.b = diff
+  end
+  return HSBtoHEX(color_hsb)
+end
+
+return M
